ea732f1fc475c692c9f7e57194a38c4ead856f98
carloshtbraga <carloshtbraga@gmail.com>
2023-04-09 14:15:02 -0300

    oi

---
+++ curso-battisti/fundamentos/pratica-args/index.js
@@ -0,0 +1,15 @@
+// externo 
+const minimist = require('minimist')
+
+
+// interno
+
+const soma = require('./soma').soma
+
+const args = minimist(process.argv.slice(2))
+
+const a = Number(args['a'])
+const b = Number(args['b'])
+
+soma(a,b)
+
