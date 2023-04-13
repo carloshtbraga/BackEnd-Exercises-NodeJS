b8dd014743675a83c5d6424ee7ac103f74b7ea22
carloshtbraga <carloshtbraga@gmail.com>
2023-04-13 14:28:10 -0300

    treinos

--- dia5/trybecash-api/src/app.js
+++ dia5/trybecash-api/src/app.js
@@ -1,9 +1,9 @@
 // src/app.js
-
+const peopleRoutes = require('./routes/peopleRoutes');
 const express = require('express');
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/people', peopleRoutes)
 module.exports = app;
