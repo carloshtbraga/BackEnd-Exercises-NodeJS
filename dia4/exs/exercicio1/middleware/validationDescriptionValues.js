b10779e88bb11e0114e116c475e33bdfcc7cf15c
carloshtbraga <carloshtbraga@gmail.com>
2023-04-13 11:13:23 -0300

    Treinos

--- dia4/exs/exercicio1/middleware/validationDescriptionValues.js
+++ dia4/exs/exercicio1/middleware/validationDescriptionValues.js
@@ -12,7 +12,6 @@ const validationDescriptionValues = (req, res, next) => {
   }
   const classifications = ['Fácil', 'Médio', 'Difícil'];
   if (!classifications.includes(difficulty) ) {
-
     return res
       .status(400)
       .json({ message: 'O campo difficulty deve ser \'Fácil\', \'Médio\' ou \'Difícil\'' });
