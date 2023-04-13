19ddbe8dd2896829d7868b8234b79e66a98bf8ab
carloshtbraga <carloshtbraga@gmail.com>
2023-04-13 14:20:05 -0300

    treinos

--- dia5/trybecash-api/src/server.js
+++ dia5/trybecash-api/src/server.js
@@ -1,12 +1,12 @@
 const app = require('./app');
 const connection = require('./db/connection');
 
-const PORT = 3001;
+const PORT = 3002;
 
 app.listen(PORT, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
 
-  // O código abaixo é para testarmos a comunicação com o MySQL
+//   O código abaixo é para testarmos a comunicação com o MySQL
   const [result] = await connection.execute('SELECT 1');
   if (result) {
     console.log('MySQL connection OK');
